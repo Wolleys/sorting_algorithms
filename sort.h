a23c158e28a1fa878221ca018ec552e9d4c9802f
Wolleys <wolleysmigaya@gmail.com>
2023-01-24 15:16:36 +0000

    Sort an array of integers in ascending order > Using the Shell sort algorithm > Prototype: void shell_sort(int *array, size_t size);

--- sort.h
+++ sort.h
@@ -25,6 +25,9 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void _qsort(int *a, int low, int high, int size);
+void shell_sort(int *array, size_t size);
+void _ssort(int *a, int size, int n);
+
 
 
 
