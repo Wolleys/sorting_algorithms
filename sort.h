6f34df1a15397fd7fc8ca05aa993b94bce614c2f
Wolleys <wolleysmigaya@gmail.com>
2023-01-24 15:47:01 +0000

    Sort an array of integers in ascending order > Using the Merge sort algorithm > Prototype: void merge_sort(int *array, size_t size);

--- sort.h
+++ sort.h
@@ -29,6 +29,7 @@ void shell_sort(int *array, size_t size);
 void _ssort(int *a, int size, int n);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
 
 
 
